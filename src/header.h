5d020edff74e1e69890a81c58d38056102023822
Fi-77-70 <fi-77-70@gmail.com>
2024-09-06 19:06:51 +0100

    graphics and transparency (has segfault when exit)

--- src/header.h
+++ src/header.h
@@ -28,5 +28,5 @@ void	free_matrix(char **line);
 void	game_loop(char **map, int y, int x);
 int	close_game(int key_code, t_gui *gui);
 void	move_player(t_gui *gui, int y, int x);
-
+void	graphics(t_gui *gui, t_img *floor, t_img *wall, t_img *player);
 #endif
--- src/logic/game_logic.c
+++ src/logic/game_logic.c
@@ -19,6 +19,18 @@ void	move_player(t_gui *gui, int y, int x)
 
 int	close_game(int key_code, t_gui *gui)
 {
+	t_img   floor;
+        t_img   wall;
+        t_img   player;
+        int     wid;
+        int     hei;
+
+        floor.img = mlx_xpm_file_to_image(gui->mlx, "../imgs/floor.xpm", &wid, &hei);
+        floor.addr = mlx_get_data_addr(floor.img, &floor.bits_p_pixel, &floor.line_len, &floor.endian);
+        wall.img = mlx_xpm_file_to_image(gui->mlx, "../imgs/wall.xpm", &wid, &hei);
+        wall.addr = mlx_get_data_addr(wall.img, &wall.bits_p_pixel, &wall.line_len, &wall.endian);
+        player.img = mlx_xpm_file_to_image(gui->mlx, "../imgs/wizard.xpm", &wid, &hei);
+        player.addr = mlx_get_data_addr(player.img, &player.bits_p_pixel, &player.line_len, &player.endian);
 	if(key_code == XK_Escape)
 		mlx_destroy_window(gui->mlx, gui->win);
 	if (key_code == XK_Right)
@@ -29,6 +41,7 @@ int	close_game(int key_code, t_gui *gui)
 		move_player(gui, -1, 0);
 	if (key_code == XK_Down)
 		move_player(gui, 1, 0);
+	graphics(gui, &floor, &wall, &player);
 	return (0);
 }
 
@@ -38,26 +51,28 @@ void	put_tiles(t_img *buffer, t_img *tile, int y, int x)
 	char	*src;
 	int	a;
 	int	b;
+	int	temp;
 
+	temp = x;
 	a = 0;
-	b = 0;	
-	dst = buffer->addr + (y * buffer->line_len  + x * (buffer->bits_p_pixel / 8));
-	src = tile->addr + (a * tile->line_len + b * (tile->bits_p_pixel / 8));
-	while(*src)
+	b = 0;
+	while(a < 32)
 	{
 		if (b == 32)
 		{
 			a++;
+			y++;
 			b = 0;
+			x = temp;
 		}
 		if(a == 32)
 			break;
-		*(unsigned int*)dst = *(unsigned int*)src;
-		b++;
-		y += a;
-		x += b;
 		dst = buffer->addr + (y * buffer->line_len  + x * (buffer->bits_p_pixel / 8));
-		src = tile->addr + (a * tile->line_len + b * (tile->bits_p_pixel / 8));
+                src = tile->addr + (a * tile->line_len + b * (tile->bits_p_pixel / 8));
+		if(*src)
+			*(unsigned int*)dst = *(unsigned int*)src;
+		b++;
+		x++;
 	}
 }
 
@@ -75,9 +90,9 @@ void	graphics(t_gui *gui, t_img *floor, t_img *wall, t_img *player)
 		x = -1;
 		while(gui->map[y][++x])
 		{
-			if (gui->map[y][x] == '0')
+			if (gui->map[y][x] == '0' || gui->map[y][x] == 'C' || gui->map[y][x] == 'E' || gui->map[y][x] == 'P')
 				put_tiles(&buffer, floor, y * 32, x * 32);
-			if (gui->map[y][x] == '1' || gui->map[y][x] == 'C' || gui->map[y][x] == 'E')
+			if (gui->map[y][x] == '1')
                                 put_tiles(&buffer, wall, y * 32, x * 32);
 			if (gui->map[y][x] == 'P')
                                 put_tiles(&buffer, player, y * 32, x *32);
@@ -90,26 +105,13 @@ void	graphics(t_gui *gui, t_img *floor, t_img *wall, t_img *player)
 void	game_loop(char **map, int y, int x)
 {
 	t_gui	gui;
-	t_img	floor;
-	t_img	wall;
-	t_img	player;
-	int	wid;
-	int	hei;
 
 	gui.mlx = mlx_init();
-	floor.img = mlx_xpm_file_to_image(gui.mlx, "../../imgs/floor.xpm", &wid, &hei);
-	floor.addr = mlx_get_data_addr(floor.img, &floor.bits_p_pixel, &floor.line_len, &floor.endian);
-	wall.img = mlx_xpm_file_to_image(gui.mlx, "../../imgs/wall.xpm", &wid, &hei);
-	wall.addr = mlx_get_data_addr(wall.img, &wall.bits_p_pixel, &wall.line_len, &wall.endian);
-	player.img = mlx_xpm_file_to_image(gui.mlx, "../../imgs/wizard.xpm", &wid, &hei);
-	player.addr = mlx_get_data_addr(player.img, &player.bits_p_pixel, &player.line_len, &player.endian);
-
 	gui.map = map;
 	gui.p_x = x;
 	gui.p_y = y;
 	gui.win = mlx_new_window(gui.mlx, 864, 320, "Hello");
 	mlx_hook(gui.win, 3, (1L<<1), close_game, &gui);
-	graphics(&gui, &floor, &wall, &player);
 	mlx_loop(gui.mlx);
 
 	return;
